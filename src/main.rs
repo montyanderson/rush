5a56552e51984ac2a3a924a794616700fbe8a421
Monty Anderson <work@montyanderson.net>
2021-06-27 16:09:58 +0100

    exec, cd, exit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::io::{Write, stdin, stdout};
+use std::process::Command;
+use std::path::Path;
+use std::env;
+
+fn main() {
+	loop {
+		print!("> ");
+		stdout().flush();
+	
+		let mut input = String::new();
+		stdin().read_line(&mut input).unwrap();
+
+	    let mut parts = input.trim().split_whitespace();
+	    let command = parts.next().unwrap();
+	    let args = parts;
+
+		match command {
+			"cd" => {
+				let new_dir = args.peekable().peek().map_or("/", |x| *x);
+				let root = Path::new(new_dir);
+
+				if let Err(e) = env::set_current_dir(&root) {
+					eprintln!("{}", e);
+				}
+			},
+			"exit" => return,
+			command => {
+				let child = Command::new(command)
+		    	.args(args)
+		        .spawn();
+
+		        match child {
+		        	Ok(mut child) => {
+		        		child.wait();
+		        	},
+		        	Err(e) => eprintln!("{}", e)
+		        }
+			}
+		}
+	}
+}
